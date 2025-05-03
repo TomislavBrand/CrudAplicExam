75f41db1e90dc2cec8bd4a73b1eb0442ae4bd088
TomislavBrand <tomislavbrand5@gmail.com>
2025-05-03 12:46:20 +0300

    Add project files.

---
+++ CrudAplicExam/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudAplicExam.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете име на продукта")]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000, ErrorMessage = "Цената трябва да е между 0.01 и 10 000")]
+        public decimal Price { get; set; }
+    }
+}
